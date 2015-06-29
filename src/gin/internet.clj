4fcaed8825eb3d4cd4b715093d44e5f162844469
Christopher Mark Gore <cgore@cgore.com>
2015-06-29 13:29:48 -0500

    Adding domain names.

--- src/gin/internet.clj
+++ src/gin/internet.clj
@@ -1,6 +1,7 @@
 (ns gin.internet
   "Generates all sorts of Internet-related things."
-  (:require [clojure.test.check.generators :as gen]))
+  (:require [clojure.test.check.generators :as gen]
+            [clojure.string :refer [join]]))
 
 (def ipv4-address
   "This generates IPv4 address strings, things like \"192.168.0.1\"."
@@ -12,8 +13,35 @@
 
 ;;; TODO: add support for generating shortened IPv6 addresses.
 (def ipv6-address
-  "This generates IPv6 address strings, things like \"b6b5:4d8c:0d1f:4dfc:9b51:e8ba:3a39:4621\"."
-  (gen/fmap #(clojure.string/join ":" %)
-            (gen/vector (gen/fmap #(format "%04x" %)
+  "This generates IPv6 address strings,
+  things like \"b6b5:4d8c:0d1f:4dfc:9b51:e8ba:3a39:4621\"."
+  (gen/fmap (partial join ":")
+            (gen/vector (gen/fmap (partial format "%04x")
                                   (gen/choose 0x0 0xffff))
                         8)))
+
+(def valid-domain-name-characters
+  "These are all of the characters that are valid for a domain name label."
+  (seq "abcdefghijklmnopqrstuvwxyz0123456789-"))
+
+(def valid-domain-name-endcap-characters
+  "These are all of the characters that are valid for the first or last
+  character of a domain name label."
+  (seq "abcdefghijklmnopqrstuvwxyz0123456789"))
+
+(def domain-name-label
+  "This generates domain name labels, the components of a domain name. For
+  example, in the domain name 'www.foo.com', there are three labels, 'www',
+  'foo', and 'com'."
+  (gen/such-that #(<= 1 (count %))
+                 (gen/fmap join
+                           (gen/tuple (gen/elements valid-domain-name-endcap-characters)
+                                      (gen/fmap join (gen/such-that #(<= 1 (count %) 61)
+                                                                    (gen/vector (gen/elements valid-domain-name-characters))))
+                                      (gen/elements valid-domain-name-endcap-characters)))))
+
+(def domain-name
+  "This generates a complete domain name, such as 'www.foo.com'."
+  (gen/such-that #(<= 1 (count %) 253)
+                 (gen/fmap (partial join ".")
+                           (gen/vector domain-name-label))))
