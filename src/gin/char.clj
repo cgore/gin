36ecfa013feb1405f73760f622cbd85824e0141a
Christopher Mark Gore <cgore@cgore.com>
2015-06-29 15:26:07 -0500

    Adding generators for hex number characters.

--- src/gin/char.clj
+++ src/gin/char.clj
@@ -11,3 +11,12 @@
   "Generate uppercase alpha characters."
   (gen/fmap char
             (gen/one-of [(gen/choose 65 90)])))
+
+(def hex-lower
+  (gen/elements (seq "0123456789abcdef")))
+
+(def hex-upper
+  (gen/elements (seq "0123456789ABCDEF")))
+
+(def hex
+  (gen/one-of [hex-lower hex-upper]))
