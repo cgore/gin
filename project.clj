78f78d8947320d2c34ae48be0064eec5d84abae1
Christopher Mark Gore <cgore@cgore.com>
2015-10-01 14:50:55 -0500

    Depedency update.

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject gin "0.3.2"
+(defproject gin "0.3.3"
   :description "All the generators you need and/or want."
   :url "https://github.com/cgore/gin"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/test.check "0.7.0"]])
+                 [org.clojure/test.check "0.8.2"]])
