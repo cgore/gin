6711001a9a99fe321132d2eff524936d11bed3df
Christopher Mark Gore <cgore@cgore.com>
2016-01-11 17:14:36 -0600

    Float generator fix to work with the new double.

--- src/gin/core.clj
+++ src/gin/core.clj
@@ -6,6 +6,13 @@
   "This generates a java.lang.Double."
   gen/double)
 
+(defn float<-double [d]
+  (cond
+    (= Double/NEGATIVE_INFINITY d) Float/NEGATIVE_INFINITY
+    (= Double/POSITIVE_INFINITY d) Float/POSITIVE_INFINITY
+    :else (try (clojure.core/float d)
+               (catch Exception _ (clojure.core/float 0.0)))))
+
 (def float
   "This generates a java.lang.Float."
-  (gen/fmap clojure.core/float double))
+  (gen/fmap float<-double double))
