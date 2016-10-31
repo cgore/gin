b7f09493a72aad8c2da974cd07ef85f02afe571a
Christopher Mark Gore <cgore@cgore.com>
2016-10-31 12:15:50 -0500

    Bumping clojure to 1.8

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject gin "0.3.5"
+(defproject gin "0.4.0"
   :description "All the generators you need and/or want."
   :url "https://github.com/cgore/gin"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/test.check "0.9.0"]])
